0f7cdd951c75a62a4f952784c117615dc693c91c
Colin Atkinson <colin.william.atkinson@gmail.com>
2018-12-24 23:18:34 -0500

    Get loops working, I think!

--- bf.asm
+++ bf.asm
@@ -4,10 +4,15 @@ extern puts
 extern scanf
 
 SECTION .data
-bf_script:    db "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.---.+++++++.", 0
+;bf_script:    db "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.---.+++++++.>,+.", 0
+;bf_script: db "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++[.]", 0
+;bf_script: db "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.", 0
+;bf_script: db "++++++++++++++++++++[>+.]", 0
+bf_script: db ",[.-[-->++<]>+]", 0
 fmt: db "%s", 10, 0
-bf_mem: times 512 db 0
+bf_mem: times 16384 db 0
 term_delim: db "> ", 0
+bf_stack: times 512 db 0
 
 
 SECTION .text
@@ -21,6 +26,7 @@ push ebp
 mov ebp, esp
 
 mov eax, bf_script
+mov edi, bf_stack
 mov ecx, bf_mem
 
 putc_loop:
@@ -44,11 +50,79 @@ jz incr_dp
 cmp dl, '-'
 jz decr_dp
 
+cmp dl, '['
+jz loop_start
+
+cmp dl, ']'
+jz loop_end
+
+jmp instr_end
+
+loop_start:
+mov dl, [ecx]
+cmp dl, 0
+jz loop_start_zero
+
+mov [edi], eax
+add edi, 4
+jmp instr_end
+
+loop_start_zero:
+xor edx, edx
+.loop:
+inc eax
+mov dl, byte[eax]
+cmp dl, '['
+jnz .not_new_nest
+inc dh
+.not_new_nest:
+cmp dl, ']'
+jnz .not_close
+dec dh
+.not_close:
+cmp dh, 0
+jnz .loop
+jmp instr_end
+
+loop_end:
+mov dl, [ecx]
+;cmp dl, 0
+;jz instr_end
+;jnz loop_end_nonzero
+;jmp instr_end
+
+sub edi, 4
+
+cmp dl, 0
+jz instr_end
+
+mov eax, [edi]
+;inc eax
+dec eax
+jmp instr_end
+
+loop_end_nonzero:
+xor edx, edx
+.loop:
+dec eax
+mov dl, byte[eax]
+cmp dl, ']'
+jnz .not_new_nest
+inc dh
+.not_new_nest:
+cmp dl, '['
+jnz .not_open
+dec dh
+.not_open:
+cmp dh, 0
+jnz .loop
+;inc eax
 jmp instr_end
 
 putc:
 ; Save eax and ecx before putchar
 push eax
+push edi
 push ecx
 ; Clear ebx
 xor ebx, ebx
@@ -61,6 +135,8 @@ call putchar
 add esp, 4
 ; Restore the bf instruction pointer
 pop ecx
+; Restore the bf stack pointer
+ pop edi
 ; Restore the bf memory pointer
 pop eax
 ; Continue the instruction loop
@@ -70,6 +146,8 @@ jmp instr_end
 getc:
 ; Save the brainfuck IP
 push eax
+; Save the bf stack
+push edi
 ; Save the brainfuck mem pointer
 push ecx
 ; Call getchar to read input
@@ -82,6 +160,8 @@ jz .getc_loop
 pop ecx
 ; Save the resulting byte to bf mem
 mov [ecx], al
+; Restore ebx
+pop edi
 ; Restore eax
 pop eax
 ; Continue
