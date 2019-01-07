cee0c2ddd0a6febec8470c77a64d01e5db139f67
Colin Atkinson <colin.william.atkinson@gmail.com>
2019-01-07 00:20:02 -0500

    Allow reading from file

--- tests/test.c
+++ tests/test.c
@@ -3,11 +3,28 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 extern int bf_interp(const char *script);
 
 int main(int argc, char **argv)
 {
+    if (argc > 1) {
+        // TODO(colin): Error handling
+        FILE *f = fopen(argv[1], "r");
+        fseek(f, 0, SEEK_END);
+        long off = ftell(f);
+        fseek(f, 0, SEEK_SET);
+
+        char *buf = (char *)calloc(off + 1, sizeof(char));
+        fread(buf, sizeof(char), off, f);
+
+        int ret = bf_interp(buf);
+        free(buf);
+
+        return ret;
+    }
+
     bf_interp(",[.-[->+<]>+]");
 
     printf("\n\n");
