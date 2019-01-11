e4cfbff3648210db1529573967861b7d13504260
Colin Atkinson <colin.william.atkinson@gmail.com>
2019-01-11 01:23:39 -0500

    Add error checking to test script

--- test.sh
+++ test.sh
@@ -4,13 +4,23 @@ set -e
 cyan="\033[0;36m"
 nc="\033[0m"
 
+function err_check {
+    echo -e ${1}
+
+    if [[ ${1} != "Hello World! 255" ]]; then
+        >&2 echo "Invalid output"
+        exit 1
+    fi
+}
+
 echo -e "${cyan}Building 64-bit${nc}"
 mkdir -p build64
 cd build64
 cmake .. -DBITS=64
 cmake --build .
 echo -e "${cyan}Running 64-bit${nc}"
-./c-test ../bitwidth.bf
+val=$(./c-test ../bitwidth.bf)
+err_check "$val"
 cd ..
 echo
 
@@ -20,7 +30,8 @@ cd build32
 cmake .. -DBITS=32
 cmake --build .
 echo -e "${cyan}Running 32-bit${nc}"
-./c-test ../bitwidth.bf
+val=$(./c-test ../bitwidth.bf)
+err_check "$val"
 cd ..
 echo
 
@@ -30,4 +41,5 @@ cd build16
 cmake .. -DBITS=16
 cmake --build .
 echo -e "${cyan}Running 16-bit${nc}"
-dosemu -dumb ./dos/dos_test.com
+val=$(dosemu -dumb ./dos/dos_test.com)
+err_check "$val"
